ca6fe2b0e3c6dff9fe4a74f63f1873269df44fb9
Gauri Pethkar <gauri.pethkar7@gmail.com>
2022-05-19 19:15:37 +0530

    Commit

---
+++ LandRegistry.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.8.0;
+
+import "./ownable.sol";
+
+abstract contract LandRegistry is Ownable {
+
+    string private _country;
+    string private _city;
+    string private _landaddress;
+    string private _latitude;
+    string private _longitude;
+
+
+    constructor(string memory country, string memory city, string memory landaddress, string memory latitude, string memory longitude) 
+    {
+
+        _country = country;
+        _city = city;
+        _landaddress = landaddress;
+        _latitude = latitude;
+        _longitude = longitude;
+
+    }
+
+    function country() public view returns(string memory){
+        return _country;
+    }
+
+    function city() public view returns(string memory){
+        return _city;
+    }
+
+    function landaddress() public view returns(string memory){
+        return _landaddress;
+    }
+
+    function latitude() public view returns(string memory){
+        return _latitude;
+    }
+
+    function longitude() public view returns(string memory){
+        return _longitude;
+    }
+
+}
